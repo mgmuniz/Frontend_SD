96d732db90a9b768cb6612e5bf55831264136b1c
Victor Augusto <vaas@icomp.ufam.edu.br>
2019-06-11 19:04:37 -0400

    adicionado o pacote de models para comunicação com a api

---
+++ app/src/main/java/com/example/nutrionall/models/UserLogin.java
@@ -0,0 +1,22 @@
+package com.example.nutrionall.models;
+
+public class UserLogin {
+    String email;
+    String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
