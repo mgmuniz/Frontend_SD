04199dfd5a254ee100bf7653b612eb652bb05748
Victor Augusto Andrade Silva <vaas@icomp.ufam.edu.br>
2019-06-13 23:45:52 -0400

    Adicionado o envio de imagens para a api ao cadastrar usuário

--- app/src/main/java/com/example/nutrionall/activity/CadastroActivity.java
+++ app/src/main/java/com/example/nutrionall/activity/CadastroActivity.java
@@ -1,30 +1,34 @@
 package com.example.nutrionall.activity;
 
 import android.content.Context;
-import android.content.Intent;
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.nutrionall.R;
-import com.example.nutrionall.api.AuthService;
 import com.example.nutrionall.api.UserService;
-import com.example.nutrionall.models.AuthUser;
 import com.example.nutrionall.models.UserCadastro;
-import com.example.nutrionall.models.UserLogin;
 import com.example.nutrionall.utils.Consts;
 import com.example.nutrionall.utils.Validate;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.lang.ref.Reference;
 
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -38,6 +42,7 @@ public class CadastroActivity extends AppCompatActivity {
     private EditText editCadastroPassword;
     private EditText editCadastroEmail;
     private EditText editCadastroDataNascimento;
+    private ImageView editCadastroImgUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,13 +53,61 @@ public class CadastroActivity extends AppCompatActivity {
                 .baseUrl(Consts.API_BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
-    }
 
-    public void cadastrar(View view) {
+        // inicializa variáveis dos componentes
         editCadastroName = findViewById(R.id.editCadastroName);
         editCadastroPassword = findViewById(R.id.editCadastroPassword);
         editCadastroEmail = findViewById(R.id.editCadastroEmail);
         editCadastroDataNascimento = findViewById(R.id.editCadastroDataNascimento);
+        editCadastroImgUser = findViewById(R.id.editCadastroImgUser);
+    }
+
+    private byte[] imgToString(){
+        editCadastroImgUser.setDrawingCacheEnabled(true);
+        editCadastroImgUser.buildDrawingCache();
+
+            // recupera o bitmap da imagemd a ser enviada
+        Bitmap bitmap = editCadastroImgUser.getDrawingCache();
+
+        // comprime o bitmap para algum formato de imagem
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
+
+        // converte o byteArrayOutputStream em array
+        byte[] imgByte = byteArrayOutputStream.toByteArray();
+
+        return imgByte;
+    }
+
+    private UserCadastro getUserCadastro(){
+        UserCadastro newUser = new UserCadastro();
+        newUser.setName(editCadastroName.getText().toString());
+        newUser.setEmail(editCadastroEmail.getText().toString());
+        newUser.setGender("false");
+        newUser.setPassword(editCadastroPassword.getText().toString());
+        newUser.setDateOfBirth(editCadastroDataNascimento.getText().toString());
+
+        return newUser;
+    }
+
+    private MultipartBody.Part multipart() throws IOException {
+        File filesDir = getApplicationContext().getFilesDir();
+        File file = new File(filesDir, "imageUser" + ".jpg");
+
+        byte[] img = imgToString();
+
+        FileOutputStream fos = new FileOutputStream(file);
+        fos.write(img);
+        fos.flush();
+        fos.close();
+
+        RequestBody reqFile = RequestBody.create(MediaType.parse("image/*"), file);
+        MultipartBody.Part body = MultipartBody.Part.createFormData("urlImg", file.getName(), reqFile);
+
+        return body;
+    }
+
+    public void cadastrar(View view) {
 
         // valida campos de edição
         Context c = getApplicationContext();
@@ -63,17 +116,19 @@ public class CadastroActivity extends AppCompatActivity {
                         Validate.validateNotExistFieldOrError(editCadastroPassword, "Preencha sua senha!", c) &&
                         Validate.validateNotExistFieldOrError(editCadastroDataNascimento, "Preencha sua data de nascimento!", c)
         ) {
+            // recupera as informações do usuário dos campos de edição
+            UserCadastro newUser = getUserCadastro();
 
-            UserCadastro newUser = new UserCadastro();
-            newUser.setName(editCadastroName.getText().toString());
-            newUser.setEmail(editCadastroEmail.getText().toString());
-            newUser.setGender("false");
-            newUser.setPassword(editCadastroPassword.getText().toString());
-            newUser.setDateOfBirth(editCadastroDataNascimento.getText().toString());
+            RequestBody name = RequestBody.create(MediaType.parse("text/plain"), "urlImg");
+            MultipartBody.Part urlImg = null;
+            try {
+                urlImg = multipart();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
             UserService serviceSignUp = retrofit.create(UserService.class);
-            Call<UserCadastro> call = serviceSignUp.cadastrar(newUser);
-
+            Call<UserCadastro> call = serviceSignUp.cadastrar(urlImg, newUser);
             call.enqueue(new Callback<UserCadastro>() {
                 @Override
                 public void onResponse(Call<UserCadastro> call, Response<UserCadastro> response) {
@@ -84,9 +139,7 @@ public class CadastroActivity extends AppCompatActivity {
                         } catch (InterruptedException e) {
                             e.printStackTrace();
                         }
-                        Intent intent = new Intent(CadastroActivity.this, LoginActivity.class);
-                        startActivity(intent);
-                    }else{
+                    } else {
                         try {
                             // mensagens do servidor
                             JSONObject x = new JSONObject(response.errorBody().string());
--- app/src/main/java/com/example/nutrionall/api/UserService.java
+++ app/src/main/java/com/example/nutrionall/api/UserService.java
@@ -2,12 +2,20 @@ package com.example.nutrionall.api;
 
 import com.example.nutrionall.models.UserCadastro;
 
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.Multipart;
 import retrofit2.http.POST;
+import retrofit2.http.Part;
 
 public interface UserService {
     // cadastro de usuário
+    @Multipart
     @POST("/user")
-    Call<UserCadastro> cadastrar(@Body UserCadastro user);
+    Call<UserCadastro> cadastrar(
+            @Part MultipartBody.Part urlImg,
+            @Part("user") UserCadastro user
+    );
 }
