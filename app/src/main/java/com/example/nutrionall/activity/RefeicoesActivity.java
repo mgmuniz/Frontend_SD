2ff4aef2a68cd0aa93dbb5dfd10ff776daf8fae4
Unknown <dlzf@icomp.ufam.edu.br>
2019-06-15 16:08:36 -0400

    Inicio da tela de refeições

---
+++ app/src/main/java/com/example/nutrionall/activity/RefeicoesActivity.java
@@ -0,0 +1,15 @@
+package com.example.nutrionall.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.nutrionall.R;
+
+public class RefeicoesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_refeicoes);
+    }
+}
