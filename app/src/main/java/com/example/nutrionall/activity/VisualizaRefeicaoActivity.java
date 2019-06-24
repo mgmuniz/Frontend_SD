4f736f6382ce4b20705dee3c51e78f69a752b361
Unknown <dlzf@icomp.ufam.edu.br>
2019-06-24 13:32:21 -0400

    Pequenos ajustes no layout visualiza refeicao

--- app/src/main/java/com/example/nutrionall/activity/VisualizaRefeicaoActivity.java
+++ app/src/main/java/com/example/nutrionall/activity/VisualizaRefeicaoActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.ImageButton;
 
 import com.example.nutrionall.R;
 import com.example.nutrionall.activity.adapterVisualizaRefeicao.MyFragPageAdapterVisuRefeicao;
@@ -14,6 +15,7 @@ public class VisualizaRefeicaoActivity extends AppCompatActivity {
     private TabLayout mTabLayout;
     private ViewPager mViewPager;
 
+    private ImageButton imgbuttonFavoriteRefeicao;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
